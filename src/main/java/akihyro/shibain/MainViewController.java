12eabbf3694044d64c7c9e2c85e6eeec84f7800c
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-02-18 23:19:31 +0900

    ユーザID/パスワードを入力可能にする

--- src/main/java/akihyro/shibain/MainViewController.java
+++ src/main/java/akihyro/shibain/MainViewController.java
@@ -3,10 +3,18 @@ package akihyro.shibain;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.controlsfx.control.PopOver;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
 
 /**
@@ -15,25 +23,39 @@ import javafx.scene.web.WebView;
 public class MainViewController implements Initializable {
 
     /**
+     * ルート。
+     */
+    @FXML
+    private Pane root;
+
+    /**
+     * 設定ポップオーバー。
+     */
+    @FXML
+    private PopOver configPopOver;
+
+    /**
      * WEBビュー。
      */
     @FXML
     private WebView webView;
 
     /**
+     * ユーザIDフィールド。
+     */
+    @FXML
+    private TextField userIdField;
+
+    /**
+     * パスワードフィールド。
+     */
+    @FXML
+    private PasswordField passwordField;
+
+    /**
      * WEBエンジン。
      */
     private TsubaisoController tsubaisoController;
-
-    /**
-     * ユーザID。
-     */
-    private final String id = "user-id";
-
-    /**
-     * パスワード。
-     */
-    private final String password = "password";
 
     /** {@inheritDoc} */
     @Override
@@ -42,13 +64,32 @@ public class MainViewController implements Initializable {
     }
 
     /**
+     * ユーザIDを取得する。
+     *
+     * @return ユーザID。
+     */
+    public String getUserId() {
+        return userIdField.getText();
+    }
+
+    /**
+     * パスワードを取得する。
+     *
+     * @return パスワード。
+     */
+    public String getPassword() {
+        return passwordField.getText();
+    }
+
+    /**
      * 出勤する。
      *
+     * @param event イベント。
      * @throws Exception エラーが発生した場合。
      */
-    public void checkin() throws Exception {
+    public void checkin(ActionEvent event) throws Exception {
         tsubaisoController.loadLoginPage()
-                .thenCompose((v) -> tsubaisoController.login(id, password))
+                .thenCompose((v) -> tsubaisoController.login(getUserId(), getPassword()))
                 .thenCompose((v) -> tsubaisoController.punchIn())
                 .thenRun(() -> {
                     Alert alert = new Alert(AlertType.INFORMATION);
@@ -61,11 +102,12 @@ public class MainViewController implements Initializable {
     /**
      * 退勤する。
      *
+     * @param event イベント。
      * @throws Exception エラーが発生した場合。
      */
-    public void checkout() throws Exception {
+    public void checkout(ActionEvent event) throws Exception {
         tsubaisoController.loadLoginPage()
-                .thenCompose((v) -> tsubaisoController.login(id, password))
+                .thenCompose((v) -> tsubaisoController.login(getUserId(), getPassword()))
                 .thenCompose((v) -> tsubaisoController.punchOut())
                 .thenRun(() -> {
                     Alert alert = new Alert(AlertType.INFORMATION);
@@ -76,12 +118,21 @@ public class MainViewController implements Initializable {
     }
 
     /**
-     * 設定する。
+     * 設定の表示を切り替える。
      *
+     * @param event イベント。
      * @throws Exception エラーが発生した場合。
      */
-    public void config() throws Exception {
-        // TODO: 後で実装する
+    public void toggleConfig(ActionEvent event) throws Exception {
+        if (!configPopOver.isShowing()) {
+            Node node = Node.class.cast(event.getSource());
+            Bounds bounds = node.localToScreen(node.getBoundsInLocal());
+            configPopOver.show(root,
+                    bounds.getMinX() + bounds.getWidth() / 2,
+                    bounds.getMinY() + bounds.getHeight() - 10);
+        } else {
+            configPopOver.hide();
+        }
     }
 
 }
